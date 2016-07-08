809b36ea51fc296e2caf08b2d8b0b280f7146cb7
sciencefans <sciencefans@foxmail.com>
2016-07-09 05:21:07 +0800

    First Commit

---
+++ matlab/+caffe/get_solver.m
@@ -0,0 +1,11 @@
+function solver = get_solver(solver_file, gpu_ids)
+% solver = get_solver(solver_file)
+%   Construct a Solver object from solver_file
+
+CHECK(ischar(solver_file), 'solver_file must be a string');
+CHECK_FILE_EXIST(solver_file);
+CHECK(isnumeric(gpu_ids), 'gpu_ids should be an nonnegative array.')
+pSolver = caffe_('get_solver', solver_file, gpu_ids);
+solver = caffe.Solver(pSolver);
+
+end
