c419cb104e3d656e46094584effb7f98174dec36
zhouyucong <zhouyucong@sensetime.com>
2016-09-02 04:47:14 +0800

    fix backward of point pooling layer(atomic add); notice only average mode are fixed

--- src/caffe/layers/point_pooling_layer.cu
+++ src/caffe/layers/point_pooling_layer.cu
@@ -8,6 +8,7 @@
 #include <cfloat>
 
 #include "caffe/layers/point_pooling_layer.hpp"
+#include "caffe/util/gpu_util.cuh"
 
 using std::max;
 using std::min;
@@ -73,50 +74,52 @@ template <typename Dtype>
 __global__ void AvePointPoolForward(const int nthreads, const Dtype* bottom_data, const int ncls, 
 	int channels, const int height, const int width, const int* cls_ch, const Dtype spatial_scale,
     const Dtype* bottom_ids, const Dtype* bottom_points, const Dtype* bottom_points_conf, Dtype* top_data, const bool use_valid_channel, const Dtype conf_th) {
-  CUDA_KERNEL_LOOP(index, nthreads) {
-    // (n, cls, 1, 1) is an element in the pooled output, n represents roi id
-    int cls = index % ncls;
-    int n = index / ncls;
 
-    cls_ch += cls * 2;
-    bottom_ids += n;
-    int roi_batch_ind = bottom_ids[0];
-    int ch_len = cls_ch[1] - cls_ch[0] + 1;
+    CUDA_KERNEL_LOOP(index, nthreads) {
+	// (n, cls, 1, 1) is an element in the pooled output, n represents roi id
+	int cls = index % ncls;
+	int n = index / ncls;
+
+	const int* cls_channel = cls_ch + cls * 2;
+	int roi_batch_ind = bottom_ids[n];
+	int ch_len = cls_channel[1] - cls_channel[0] + 1;
 	
-	for (int ch = cls_ch[0]; ch <= cls_ch[1]; ch++) {
-		const Dtype* feat_map = bottom_data + (roi_batch_ind * channels + ch) * height * width;
-		const Dtype* pnt = bottom_points + (n * channels + ch) * 4;
-		const Dtype* conf = bottom_points_conf + n * channels + ch;
-        if (use_valid_channel) {
-            bool is_valid = conf[0] > conf_th;
-            if (!is_valid) {
-                ch_len--;
-                continue; // the point is absent
-            }
-        }
-        int x1 = floor(pnt[0] * spatial_scale);
-        int y1 = floor(pnt[1] * spatial_scale);
-        int x2 = ceil(pnt[2] * spatial_scale);
-        int y2 = ceil(pnt[3] * spatial_scale);
-        x1 = min(max(x1, 0), width - 1);
-        y1 = min(max(y1, 0), height - 1);
-        x2 = min(max(x2, 0), width - 1);
-        y2 = min(max(y2, 0), height - 1);
-		Dtype avgval = 0;
-		for (int h=y1; h<=y2; h++) {
-			for (int w=x1; w<=x2; w++) {
-				int ind = h * width + w;
-				avgval += feat_map[ind];
-			}
+	for (int ch = cls_channel[0]; ch <= cls_channel[1]; ch++) {
+	    const Dtype* feat_map = bottom_data + (roi_batch_ind * channels + ch) * height * width;
+	    const Dtype* pnt = bottom_points + (n * channels + ch) * 4;
+	    const Dtype* conf = bottom_points_conf + n * channels + ch;
+	    if (use_valid_channel) {
+		if (conf[0] < conf_th) {
+		    ch_len--;
+		    continue; // the point is absent
 		}
-        if (!use_valid_channel) // use all channels, use confidence as weight
-            avgval *= conf[0];
-		top_data[index] += avgval / (y2 - y1 + 1) / (x2 - x1 + 1);
+	    }
+	    int x1 = floor(pnt[0] * spatial_scale);
+	    int y1 = floor(pnt[1] * spatial_scale);
+	    int x2 = ceil(pnt[2] * spatial_scale);
+	    int y2 = ceil(pnt[3] * spatial_scale);
+	    x1 = min(max(x1, 0), width - 1);
+	    y1 = min(max(y1, 0), height - 1);
+	    x2 = min(max(x2, 0), width - 1);
+	    y2 = min(max(y2, 0), height - 1);
+
+	    Dtype avgval = 0;
+	    for (int h=y1; h<=y2; h++) {
+		for (int w=x1; w<=x2; w++) {
+		    int ind = h * width + w;
+		    avgval += feat_map[ind];
+		}
+	    }
+	    if (!use_valid_channel) { // use all channels, use confidence as weight
+		avgval *= conf[0];
+	    }
+
+	    top_data[index] += avgval / (y2 - y1 + 1) / (x2 - x1 + 1);
 	}
-    if (ch_len > 0)
-        top_data[index] /= ch_len;
-	
-  }
+	if (ch_len > 0) {
+	    top_data[index] /= ch_len;
+	}
+    }
 }
 template <typename Dtype>
 void PointPoolingLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
@@ -189,57 +192,56 @@ __global__ void AvePointPoolBackward(const int nthreads, const Dtype* top_diff,
     const int num_rois, const int ncls, const int channels,
     const int height, const int width, const int* cls_ch, const Dtype spatial_scale,
     Dtype* bottom_diff, const Dtype* bottom_ids, const Dtype* bottom_points, const Dtype* bottom_points_conf, const bool use_valid_channel, const Dtype conf_th) {
-  CUDA_KERNEL_LOOP(index, nthreads) {
-    // (n, cls, 1, 1) is an element in the pooled output, n represents roi id
-    int cls = index % ncls;
-    int n = index / ncls;
 
-    cls_ch += cls * 2;
-    bottom_ids += n;
-    int roi_batch_ind = bottom_ids[0];
-    int ch_len = cls_ch[1] - cls_ch[0] + 1;
-	
-    if (use_valid_channel) {
-        for (int ch = cls_ch[0]; ch <= cls_ch[1]; ch++) {
-            const Dtype* conf = bottom_points_conf + n * channels + ch;
-            bool is_valid = conf[0] > conf_th;
-            if (!is_valid) {
-                ch_len--;
-            }
-        }
-    }
-    
-	for (int ch = cls_ch[0]; ch <= cls_ch[1]; ch++) {
-		Dtype* diff = bottom_diff + (roi_batch_ind * channels + ch) * height * width;
-		const Dtype* pnt = bottom_points + (n * channels + ch) * 4;
+    CUDA_KERNEL_LOOP(index, nthreads) {
+    // (n, cls, 1, 1) is an element in the pooled output, n represents roi id
+	int cls = index % ncls;
+	int n = index / ncls;
+
+	const int* cls_channel = cls_ch + cls * 2;
+	int roi_batch_ind = bottom_ids[n];
+	int ch_len = cls_channel[1] - cls_channel[0] + 1;
+	    
+	if (use_valid_channel) {
+	    for (int ch = cls_channel[0]; ch <= cls_channel[1]; ch++) {
 		const Dtype* conf = bottom_points_conf + n * channels + ch;
-        if (use_valid_channel) {
-            bool is_valid = conf[0] > conf_th;
-            if (!is_valid) {
-                continue; // the point is absent
-            }
-        }
-        int x1 = floor(pnt[0] * spatial_scale);
-        int y1 = floor(pnt[1] * spatial_scale);
-        int x2 = ceil(pnt[2] * spatial_scale);
-        int y2 = ceil(pnt[3] * spatial_scale);
-        x1 = min(max(x1, 0), width - 1);
-        y1 = min(max(y1, 0), height - 1);
-        x2 = min(max(x2, 0), width - 1);
-        y2 = min(max(y2, 0), height - 1);
-		for (int h=y1; h<=y2; h++) {
-			for (int w=x1; w<=x2; w++) {
-				int ind = h * width + w;
-                if (use_valid_channel) {
-                    diff[ind] += top_diff[index] / ch_len / (y2 - y1 + 1) / (x2 - x1 + 1);
-                }
-                else {
-                    diff[ind] += top_diff[index] * conf[0] / ch_len / (y2 - y1 + 1) / (x2 - x1 + 1);
-                }
-			}
+		if (conf[0] < conf_th) {
+		    ch_len--;
 		}
+	    }
 	}
-  }
+    
+	for (int ch = cls_channel[0]; ch <= cls_channel[1]; ch++) {
+	    Dtype* diff = bottom_diff + (roi_batch_ind * channels + ch) * height * width;
+	    const Dtype* pnt = bottom_points + (n * channels + ch) * 4;
+	    const Dtype* conf = bottom_points_conf + n * channels + ch;
+	    if (use_valid_channel) {
+		if (conf[0] < conf_th) {
+		    continue; // the point is absent
+		}
+	    }
+	    int x1 = floor(pnt[0] * spatial_scale);
+	    int y1 = floor(pnt[1] * spatial_scale);
+	    int x2 = ceil(pnt[2] * spatial_scale);
+	    int y2 = ceil(pnt[3] * spatial_scale);
+	    x1 = min(max(x1, 0), width - 1);
+	    y1 = min(max(y1, 0), height - 1);
+	    x2 = min(max(x2, 0), width - 1);
+	    y2 = min(max(y2, 0), height - 1);
+	    for (int h=y1; h<=y2; h++) {
+		for (int w=x1; w<=x2; w++) {
+		    int ind = h * width + w;
+		    if (use_valid_channel) {
+			Dtype diff_val = top_diff[index] / ch_len /(y2 - y1 + 1) / (x2 - x1 + 1);
+			caffe_gpu_atomic_add(diff_val, diff + ind);
+		    } else {
+			Dtype diff_val = top_diff[index] * conf[0] / ch_len / (y2 - y1 + 1) / (x2 - x1 + 1);
+			caffe_gpu_atomic_add(diff_val, diff + ind);
+		    }
+		}
+	    }
+	}
+    }
 }
 
 template <typename Dtype>
